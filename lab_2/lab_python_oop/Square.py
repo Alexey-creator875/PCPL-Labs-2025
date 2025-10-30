34514b9ddce28211255329c107d3d22896c07ace
Alexey <lexanav1245@mail.ru>
2025-10-30 11:44:51 +0300

    add realisation for class Square

---
+++ lab_2/lab_python_oop/Square.py
@@ -0,0 +1,13 @@
+from lab_python_oop.Rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, length, color):
+        super().__init__(length, length, color)
+
+    def area(self):
+        return self.width * self.length
+    
+    def __repr__(self):
+        return 'Length: {}, Color: {}, Area: {}'.format(self.length, self.color.color, self.area())
+    
