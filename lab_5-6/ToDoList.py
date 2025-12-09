088b0122b89785faa9ab4a7cf5d5d2f2aa3d6c29
Alexey <lexanav1245@mail.ru>
2025-12-09 13:49:04 +0300

    make tests green

--- lab_5-6/ToDoList.py
+++ lab_5-6/ToDoList.py
@@ -9,52 +9,47 @@ class TaskType(Enum):
 
 
 class Task(ABC):
-    def __init__(self, status, name, task):
-        self.status = status
-        self.name = name
-        self.task = task
+    def __init__(self, description):
+        self.description = description
 
-    def check_status(self):
-        return self.status
-
-    @abstractmethod
-    def get_priority(self):
-        pass
+    # @abstractmethod
+    # def get_priority(self):
+    #     pass
 
 
 
 class SimpleTask(Task):
-    def __init__(self, status, name, task):
-        super().__init__(status, name, task)
+    def __init__(self, description):
+        super().__init__(description)
 
-    def get_priority(self):
-        return int(TaskType.SIMPLE)
-
-
-class UrgentTask(Task):
-    def __init__(self, status, name, task, deadline):
-        super().__init__(status, name, task)
-        self.deadline = deadline
-
-    def get_priority(self):
-        return int(TaskType.URGENT)
-
-class RecurringTask(Task):
-    def __init__(self, status, name, task, period):
-        super().__init__(status, name, task)
-        self.period = period
-
-    def get_priority(self):
-        return int(TaskType.RECURRING)
+    # def get_priority(self):
+    #     return int(TaskType.SIMPLE)
 
 
-class TaskFactory:
-    def create_task(self, task_type, *args, **kwargs):
-        if task_type == TaskType.URGENT:
-            return UrgentTask(*args, **kwargs)
-        elif task_type == TaskType.RECURRING:
-            return RecurringTask(*args, **kwargs)
-        elif task_type == TaskType.SIMPLE:
-            return SimpleTask(*args, **kwargs)
-        else:
-            raise ValueError(f'Unknown task type: {task_type}')
+# class UrgentTask(Task):
+#     def __init__(self, status, name, task, deadline):
+#         super().__init__(status, name, task)
+#         self.deadline = deadline
+
+#     def get_priority(self):
+#         return int(TaskType.URGENT)
+
+# class RecurringTask(Task):
+#     def __init__(self, status, name, task, period):
+#         super().__init__(status, name, task)
+#         self.period = period
+
+#     def get_priority(self):
+#         return int(TaskType.RECURRING)
+
+
+# class TaskFactory:
+#     def create_task(self, task_type, *args, **kwargs):
+#         if task_type == TaskType.URGENT:
+#             return UrgentTask(*args, **kwargs)
+#         elif task_type == TaskType.RECURRING:
+#             return RecurringTask(*args, **kwargs)
+#         elif task_type == TaskType.SIMPLE:
+#             return SimpleTask(*args, **kwargs)
+#         else:
+#             raise ValueError(f'Unknown task type: {task_type}')
